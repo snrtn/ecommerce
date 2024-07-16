155bf344e587246e0f7bafaa8df7371180d366fa
Jun <hanjun.kim.dev@gmail.com>
2024-07-16 13:30:48 +0200

    commit 16.07.2024

--- ecommerce.front/src/components/app/slideView.data.ts
+++ ecommerce.front/src/components/app/slideView.data.ts
@@ -44,7 +44,51 @@ const slides = [
     link: "/category",
     texts: "View Discounted Products",
   },
-  // 추가 슬라이드...
+  {
+    desktopImage:
+      "https://images.pexels.com/photos/27089399/pexels-photo-27089399/free-photo-of-a-woman-in-a-white-dress-is-looking-up.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    mobileImage:
+      "https://images.pexels.com/photos/27055010/pexels-photo-27055010/free-photo-of-a-woman-in-a-white-dress-standing-in-a-park.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    title: "Women's Clothing",
+    description:
+      "Enjoy men's seasonal clothing at 30% off now. Enjoy the latest styles at affordable prices.",
+    link: "/category",
+    texts: "View Discounted Products",
+  },
+  {
+    desktopImage:
+      "https://images.pexels.com/photos/26918852/pexels-photo-26918852/free-photo-of-a-woman-in-a-wedding-dress-is-posing-for-a-photo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    mobileImage:
+      "https://images.pexels.com/photos/26937698/pexels-photo-26937698/free-photo-of-a-woman-in-a-blue-dress-sitting-on-a-bed.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    title: "Women's Clothing",
+    description:
+      "Offering men's clothing at 30% off. Buy a variety of men's clothing styles at low prices.",
+    link: "/category",
+    texts: "View Product",
+  },
+
+  {
+    desktopImage:
+      "https://images.pexels.com/photos/23910722/pexels-photo-23910722/free-photo-of-a-woman-in-a-dress-and-sunglasses-standing-on-a-street.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    mobileImage:
+      "https://images.pexels.com/photos/23910717/pexels-photo-23910717/free-photo-of-light-fashion-people-woman.jpeg?auto=compress&cs=tinysrgb&w=800",
+    title: "Women's Seasonal Clothing",
+    description:
+      "Enjoy men's seasonal clothing at 30% off now. Enjoy the latest styles at affordable prices.",
+    link: "/category",
+    texts: "View Discounted Products",
+  },
+  {
+    desktopImage:
+      "https://images.pexels.com/photos/20004887/pexels-photo-20004887/free-photo-of-smiling-woman-walking-in-purple-dress.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    mobileImage:
+      "https://images.pexels.com/photos/19990819/pexels-photo-19990819/free-photo-of-brunette-in-lilac-dress-with-ruffles.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    title: "Women's Clothing",
+    description:
+      "Get up to 30% off women's seasonal clothing. Purchase trendy fashion at a low price.",
+    link: "/category",
+    texts: "View Discounted Products",
+  },
 ];
 
 export default slides;
