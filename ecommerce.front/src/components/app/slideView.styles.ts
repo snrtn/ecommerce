94930e16dd1517ebc5df673d2262ed2eb5246ed8
Jun <hanjun.kim.dev@gmail.com>
2024-07-17 02:17:43 +0200

    commit 17.07.2024

--- ecommerce.front/src/components/app/slideView.styles.ts
+++ ecommerce.front/src/components/app/slideView.styles.ts
@@ -5,7 +5,7 @@ const slideView = {
     "relative flex h-full w-full items-start md:items-center justify-center pt-4 md:pt-0",
   imageWrapper: "relative h-full w-full",
   slideImage: "h-full w-full object-cover",
-  darkOverlay: "absolute inset-0 bg-black bg-opacity-40 md:bg-opacity-30",
+  darkOverlay: "absolute inset-0 bg-black bg-opacity-30 md:bg-opacity-30",
   slideOverlay:
     "absolute inset-0 flex flex-col items-center text-center md:text-left md:items-start justify-end py-48 md:py-0 md:justify-center px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64",
   slideTitle: "text-xl md:text-3xl font-bold text-white",
