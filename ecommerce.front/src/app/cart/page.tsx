80aa12637f3b9303ef2515d747ba86fbd6d380f3
Jun <hanjun.kim.dev@gmail.com>
2024-07-18 20:20:19 +0200

    commit 18.07.2024

--- ecommerce.front/src/app/cart/page.tsx
+++ ecommerce.front/src/app/cart/page.tsx
@@ -126,11 +126,11 @@ const CartPage = () => {
   };
 
   return (
-    <div className="xl:px-34 relative left-0 top-0 z-50 min-h-screen bg-white px-4 pb-40 md:px-8 lg:px-24 2xl:px-36">
+    <div className="xl:px-34 z-50min-h-screen relative left-0 top-0 bg-white px-4 py-2 pb-40 md:px-8 lg:px-24 2xl:px-64">
       <div className="container py-5">
         <div className="mb-8 flex items-center justify-between">
-          <Link href="/" className="flex items-center gap-3">
-            <div className="text-2xl font-semibold">ecommerce</div>
+          <Link href="/" className="ml-4 flex items-center">
+            <div className="text-xl font-semibold">KIM</div>
           </Link>
         </div>
         <h1 className="mx-auto my-20 w-full text-center text-2xl font-semibold">
